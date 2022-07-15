f8a9756f009bf60c66b86d3880da4bc128e07f59
unknown <sntakirutimana72@gmail.com>
2022-07-15 15:48:26 +0200

    Implement required changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -19,24 +19,59 @@ UserProfilePanel.propTypes = {
   display: PropTypes.string.isRequired,
 };
 
+const getTabPartialUrl = (url) => (
+  url.includes('/categories') ? 'CAT' : 'BOO'
+);
+
 const Navbar = () => {
   const [visibleUser, setVisibleUser] = useState(false);
+  const [active, setActive] = useState('');
   const display = visibleUser ? 'block' : 'none';
+
+  useEffect(() => {
+    const partialUrl = getTabPartialUrl(window.location.href);
+    setActive(partialUrl);
+  }, []);
 
   const toggleUser = () => {
     setVisibleUser((prevState) => !prevState);
   };
 
+  const toggleActiveTab = ({ target }) => {
+    const activeTab = getTabPartialUrl(target.href);
+    setActive(activeTab);
+  };
+
   return (
     <nav className={styles.Navbar}>
-      <Link to="/" className={styles.Home}>Bookstore CMS</Link>
+      <Link
+        to="/"
+        onClick={toggleActiveTab}
+        className={styles.Home}
+      >
+        Bookstore CMS
+      </Link>
       <div>
         <ul className={styles.UlLinks}>
           <li>
-            <Link to="/" className={styles.Link}>BOOKS</Link>
+            <Link
+              to="/"
+              style={{ opacity: (active === 'BOO') ? 1 : 0.4 }}
+              onClick={toggleActiveTab}
+              className={styles.Link}
+            >
+              BOOKS
+            </Link>
           </li>
           <li>
-            <Link to="/categories" className={styles.Link}>CATEGORIES</Link>
+            <Link
+              to="/categories"
+              style={{ opacity: (active === 'CAT') ? 1 : 0.4 }}
+              onClick={toggleActiveTab}
+              className={styles.Link}
+            >
+              CATEGORIES
+            </Link>
           </li>
         </ul>
       </div>
